1f657b5607119631382c3642e7488e27eac8b5ef
Christopher Scott <chris.scott1607@gmail.com>
2023-02-02 15:02:46 -0600

    updated UI simplify

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+/*import './App.css'
 import Main from './components/MainComponent';
 
 
@@ -13,3 +13,25 @@ function App() {
 }
 
 export default App;
+*/
+
+import React, { Component } from 'react';
+import './App.css';
+import Banner from './components/Banner';
+import Exhibit from './components/Exhibit';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Banner bannerText="Sextant" />
+        <Exhibit name="I'm an exhibit!"></Exhibit>
+        <Exhibit name="I'm also an exhibit!"></Exhibit>
+        <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
+      </div>
+    );
+  }
+}
+
+export default App;
+
--- sextant/src/components/Banner.js
+++ sextant/src/components/Banner.js
@@ -1,11 +1,15 @@
-import React from "react"
 
-function Banner() {
-    return(
-        <React.Fragment>
-            <h4>Sextant</h4>
-        </React.Fragment>
-    )
+import React, { Component } from 'react';
+import '../Banner.css';
+
+class Banner extends Component {
+    render() {
+        return (
+            <div className="Banner">
+                <h1>{this.props.bannerText}</h1>
+            </div>
+        );
+    }
 }
 
-export default Banner 
+export default Banner;
